d617043fdb2462d6b537487e66746d9158205ad6
Furkan Öztürk <furkan___01@live.nl>
2021-01-13 17:06:19 +0100

    Firebase opzet

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div>
+      jksafhks
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Signup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { TextField, Card, Button } from '@material-ui/core';
+
+export default function Signup() {
+    return (
+        <>
+            <Card>
+                <h2>Sign up</h2>
+            </Card>
+            <div>
+                Already have an account? Log in
+            </div>
+        </>
+    )
+}
